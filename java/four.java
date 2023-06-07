ca9030e791098165f388c7b813fc27a756453253
yashwanth7337 <yash.yashwanth123@gmail.com>
2023-06-07 12:58:56 +0530

    java

---
+++ java/four.java
@@ -0,0 +1,15 @@
+
+
+public class four {
+
+    public static void main(String[] args)
+    {
+      int a=10;
+      String name="yashwanth";
+      char c='a'; 
+      System.out.println(a);
+      System.out.println(name);
+      System.out.println(c);
+    }
+    
+}
